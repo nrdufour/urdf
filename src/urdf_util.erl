19fed2c711325e89945979032722cdf832710921
Nicolas R Dufour <nrdufour@gmail.com>
2011-07-31 03:33:24 -0400

    Adding a *working* uuid generator based on the one used by Couchdb.

---
+++ src/urdf_util.erl
@@ -0,0 +1,30 @@
+
+% This file is part of erlang-json-ld released under the MIT license.
+% See the LICENSE file for more information.
+
+-module(urdf_util).
+-author("Nicolas R Dufour <nicolas.dufour@nemoworld.info>").
+
+-export([uuid/0]).
+
+uuid() ->
+    Now = {_, _, Micro} = now(),
+    Nowish = calendar:now_to_universal_time(Now),
+    Nowsecs = calendar:datetime_to_gregorian_seconds(Nowish),
+    Then = calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
+    Prefix = io_lib:format("~14.16.0b", [(Nowsecs - Then) * 1000000 + Micro]),
+    list_to_binary(Prefix ++ to_hex(crypto:rand_bytes(9))).
+
+%
+%
+%
+
+to_hex([]) ->
+    [];
+to_hex(Bin) when is_binary(Bin) ->
+    to_hex(binary_to_list(Bin));
+to_hex([H|T]) ->
+    [to_digit(H div 16), to_digit(H rem 16) | to_hex(T)].
+
+to_digit(N) when N < 10 -> $0 + N;
+to_digit(N)             -> $a + N-10.
